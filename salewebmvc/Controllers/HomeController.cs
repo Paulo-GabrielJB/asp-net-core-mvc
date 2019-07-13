99c4aac5fa6ee0a2390d7f040b1165fdb921b709
Paulo <pgj1085@gmail.com>
2019-07-13 18:01:08 -0300

    Primeiro model-view

--- salewebmvc/Controllers/HomeController.cs
+++ salewebmvc/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using salewebmvc.Models;
-
+//using salewebmvc.Models.ViewModels;
 namespace salewebmvc.Controllers
 {
     public class HomeController : Controller
---
+++ salewebmvc/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace salewebmvc.Models
+{
+    public class Department
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+
+    }
+}
