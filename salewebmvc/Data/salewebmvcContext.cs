d8cff4f0ba355b18238ed6b48896e2e0a308e422
Paulo <pgj1085@gmail.com>
2019-07-14 17:44:17 -0300

    CRUD SCHAFOLD

---
+++ salewebmvc/Data/salewebmvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace salewebmvc.Models
+{
+    public class salewebmvcContext : DbContext
+    {
+        public salewebmvcContext (DbContextOptions<salewebmvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<salewebmvc.Models.Department> Department { get; set; }
+    }
+}
