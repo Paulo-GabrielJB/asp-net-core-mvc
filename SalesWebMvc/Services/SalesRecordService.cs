19ad8f6c1835566c12881f4cb243ff11b48e72f1
Paulo <pgj1085@gmail.com>
2019-08-05 21:40:29 -0300

    Implementando busca agrupada

--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -31,5 +31,21 @@ namespace SalesWebMvc.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+                result = result.Where(x => x.Date >= minDate.Value);
+
+            if (maxDate.HasValue)
+                result = result.Where(x => x.Date <= maxDate.Value);
+
+            return await result.Include(x => x.Seller) //inclui uma tabela, essa opçao vem do Microsoft.EntityFrameworkCore;
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
     }
 }
